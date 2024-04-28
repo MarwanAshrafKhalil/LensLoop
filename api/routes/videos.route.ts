16cbf664cca94e5ce36a0d8e2af506f0354cb1be
MarwanAshrafKhalil <marwan.a.k.1997@gmail.com>
2024-04-28 15:49:56 +0300

    create vidoe routes

---
+++ api/routes/videos.route.ts
@@ -0,0 +1,18 @@
+import express from "express";
+import { authJWT } from "../utils/authJWT";
+import {
+  deleteVideo,
+  getVideo,
+  getVideos,
+  test,
+  updateVideo,
+  uploadVideo,
+} from "../controllers/video.controller";
+const router = express.Router();
+
+router.get("/", test);
+router.post("/get", getVideo);
+router.post("/getAll", getVideos);
+router.post("/upload", authJWT, uploadVideo);
+router.post("/delete", authJWT, deleteVideo);
+router.post("/update", authJWT, updateVideo);
