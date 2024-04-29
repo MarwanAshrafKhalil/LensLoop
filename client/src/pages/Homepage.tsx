bf80ecdff49bee479da34dd43d8b93aa08999ba4
MarwanAshrafKhalil <marwan.a.k.1997@gmail.com>
2024-04-30 02:04:17 +0300

    Create Homepage

---
+++ client/src/pages/Homepage.tsx
@@ -0,0 +1,19 @@
+import MediaCard from "../components/MediaCard";
+import "../styles/homepage.css";
+
+export default function Homepage() {
+  return (
+    <div className="homepage">
+      <MediaCard />
+      <MediaCard />
+      <MediaCard />
+      <MediaCard />
+      <MediaCard />
+      <MediaCard />
+      <MediaCard />
+      <MediaCard />
+      <MediaCard />
+      <MediaCard />
+    </div>
+  );
+}
